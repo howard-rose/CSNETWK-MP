1e3f35c3de2366e99fc59d5ac8510d874319dae4
Joshua Permito <dwigoric@insanedevs.com>
2023-12-03 15:20:00 +0800

    feat: add client methods and client controller

--- client.py
+++ client.py
@@ -26,9 +26,151 @@ class Client:
     # Store the current connection to the server
     connection = None
 
-    def __init__(self, host, port):
-        # Create a socket
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def send(self, data):
+        """
+        This method sends data to the server.
+        :param data: The data to send to the server.
+        :return: None
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
 
+        # Send the data
+        self.connection.send(data)
+
+    def receive(self):
+        """
+        This method receives data from the server.
+        :return: The data received from the server.
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
+
+        # Receive the data
+        return self.connection.recv(1024)
+
+    def join(self, host, port, blocking=True):
+        """
+        This method connects the client to the server.
+        :param host: The host name of the server to connect to.
+        :param port: The port number of the server to connect to.
+        :param blocking: Whether the socket should be blocking.
+        :return: None
+        """
+        # Warn if already connected to a server
+        if self.connection is not None:
+            print('Already connected to a server')
+            return False
+
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.settimeout(5)
+        self.connection.setblocking(blocking)
         # Connect to the server
-        self.socket.connect((host, port))
+        self.connection.connect((host, port))
+
+    def leave(self):
+        """
+        This method closes the connection to the server.
+        :return: None
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
+
+        # Close the connection
+        self.connection.close()
+
+        # Set the connection to None
+        self.connection = None
+
+    def register(self, username):
+        """
+        This method registers the client to the server.
+        :param username: The username of the client.
+        :return: None
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
+
+        # Send the username
+        self.send(f'REGISTER {username}'.encode())
+
+    def receive_file(self, filename):
+        """
+        This method requests a file from the server.
+        :param filename: The name of the file to request.
+        :return: The contents of the file requested.
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
+
+        # Send the request
+        self.send(f'GET {filename}'.encode())
+
+        # Receive the response
+        return self.receive()
+
+    def store(self, filename):
+        """
+        This method sends a file to the server.
+        :param filename: The name of the file to send.
+        :return: None
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
+
+        # Open the file
+        file = open(filename, 'rb')
+
+        # Send the file
+        self.send(f'STORE {filename} {file.read()}'.encode())
+
+        # Close the file
+        file.close()
+
+    def get(self, filename):
+        """
+        This method receives a file from the server.
+        :param filename: The name of the file to receive.
+        :return: None
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
+
+        # Open the file
+        file = open(filename, 'wb')
+
+        # Receive the file using self.get()
+        file.write(self.receive_file(filename))
+
+        # Close the file
+        file.close()
+
+    def dir(self):
+        """
+        This method requests the list of files from the server.
+        :return: None
+        """
+        # Check if the client is connected to a server
+        if self.connection is None:
+            print('Not connected to any server')
+            return False
+
+        # Send the request
+        self.send('DIR'.encode())
+
+        # Receive the response
+        return self.receive()
--- start_client.py
+++ start_client.py
@@ -4,22 +4,148 @@ Philip Anthon Santiago
 
 CSNETWK S12
 
-This file instantiates a Client object and starts it.
+This file instantiates a Client object and starts command input handling.
 """
-
+from socket import gaierror
 from client import Client
 
-# Input the host name and port number
-host = input("Enter the host name: ")
-while True:
-    try:
-        port = int(input("Enter the port number: "))
-        break
-    except ValueError:
-        print("Invalid port number.")
+# Define command usages
+usages = {
+    '?': '?',
+    'join': 'join <host> <port>',
+    'leave': 'leave',
+    'register': 'register <handle>',
+    'store': 'store <filename>',
+    'get': 'get <filename>',
+    'exit': 'exit',
+    'dir': 'dir'
+}
 
 # Instantiate the client
-client = Client(host, port)
+client = Client()
 
-# Start the client
-pass
+
+# Implement running commands
+def run_command(command, args):
+    """
+    This method runs a command.
+    :param command: The command to run.
+    :param args: The arguments to pass to the command.
+    :return: None
+    """
+    # Handle the command
+    if command == '?':
+        # Print usage message
+        print('Usage: /<command> <args>')
+        for command, usage in usages.items():
+            print('{}: /{}'.format(command, usage))
+    elif command == 'join':
+        # Check if the number of arguments is correct
+        if len(args) != 2:
+            print(f'Usage: {usages[command]}')
+            return
+
+        # Attempt to connect to the server
+        try:
+            result = client.join(args[0], int(args[1]), blocking=True)
+            if not result:
+                return
+
+            # Print success message with host and port
+            print('Connected to {} on port {}'.format(args[0], args[1]))
+        except ConnectionRefusedError:
+            print('Connection refused')
+            client.connection = None
+        except TimeoutError:
+            print('Connection timed out')
+            client.connection = None
+        except gaierror:
+            print('Invalid host')
+            client.connection = None
+    elif command == 'leave':
+        result = client.leave()
+        if not result:
+            return
+
+        # Print success message
+        print('Disconnected from server')
+    elif command == 'register':
+        # Check if the number of arguments is correct
+        if len(args) != 1:
+            print(f'Usage: {usages[command]}')
+            return
+
+        # Attempt to register the client
+        try:
+            client.register(args[0])
+        except TypeError:
+            print('Invalid handle')
+    elif command == 'store':
+        # Check if the number of arguments is correct
+        if len(args) != 1:
+            print(f'Usage: {usages[command]}')
+            return
+
+        # Attempt to send the file
+        try:
+            client.store(args[0])
+        except FileNotFoundError:
+            print('File not found')
+    elif command == 'get':
+        # Check if the number of arguments is correct
+        if len(args) != 1:
+            print(f'Usage: {usages[command]}')
+            return
+
+        # Attempt to receive the file
+        try:
+            client.get(args[0])
+        except FileNotFoundError:
+            print('File not found')
+    elif command == 'dir':
+        # Attempt to receive the file
+        try:
+            client.dir()
+        except FileNotFoundError:
+            print('File not found')
+    else:
+        print('Invalid command')
+
+
+def main():
+    """
+    This method handles user input.
+    :return: None
+    """
+    # Loop forever
+    while True:
+        # Retrieve full string command from user
+        command = input('>>> ')
+
+        # Skip if command does not start with a slash
+        if command[0] != '/':
+            # Print usage message
+            print('Usage: /<command> <args>')
+            continue
+
+        # Remove the slash from the command
+        command = command[1:]
+
+        # Split the command into a list of arguments, result must be a
+        # two-tuple (command, args). Use regex for whitespace splitting.
+        args = command.split()
+        command = args[0]
+        args = args[1:]
+
+        # Break if the command is 'exit'
+        if command == 'exit':
+            print('Exiting...')
+            break
+
+        # Run the command
+        run_command(command, args)
+
+
+if __name__ == '__main__':
+    # Start command handler that will handle user input
+    main()
